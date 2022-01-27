c04de64ef7bf31e2ca6e536770822a82bae60f50
asslikealex <alex_todor@yahoo.com>
2022-01-27 20:17:42 +0100

    card animation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
-
-  constructor() { }
+  pickcard = false;
+  playedCard = false;
+  game = new Game();
+  currentCard: any | undefined = ''; 
+  playedcard: any | undefined = ''; 
+  constructor() {}
 
   ngOnInit(): void {
+    this.newGame();
+    console.log(this.game);
   }
 
+  newGame(){
+    this.game; //Neues Objekt erstellt
+  }
+
+  pickCard(){
+    if(!this.pickcard){ //wenn die variable false ist kann man drauf drücken
+      this.currentCard = this.game?.stack.pop();
+      console.log(this.currentCard);
+      this.pickcard = true;
+      this.playedCard = true;
+      this.game.playedCards.push(this.currentCard);
+      
+
+       setTimeout(()=>{
+          this.pickcard = false; //nach 2.5 sec. wird die variable auf false gesetzt und die karte verschwindet 
+          this.playedcard = this.game.playedCards.pop();
+      },1250) //erst nach 2.5 sec. kann man wieder drauf drücken
+    }  
+  }
 }
